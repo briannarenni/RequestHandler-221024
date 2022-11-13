7fe65a2b0ecb89b702d37b3df733739b4d5f5102
Brianna Renni <briannaxrene@icloud.com>
2022-11-12 20:10:28 -0800

    Rebuild gitignore from VS

--- RequestHandler_Data/IRepo.cs
+++ RequestHandler_Data/IRepo.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace RequestHandler_Data
 {
     public interface IRepo
     {
-        // userId
-// username
-// password
-// is_manager (BIT, DEFAULT 0)
-// pending_tickets
+
+        List<Ticket>getOpenTickets();
 
     }
 }
