f7df745749f66eb071bf7ae7b39b81d5e4e1ead5
Brianna Renni <briannaxrene@icloud.com>
2022-11-12 20:17:31 -0800

    Push

--- RequestHandler_Logic/User.cs
+++ RequestHandler_Logic/User.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace RequestHandler_Logic
 {
@@ -16,10 +19,12 @@ namespace RequestHandler_Logic
 
         public User() { }
 
-        public User(string fInitial, string lastName, string password)
+        public User(string fInitial, string lastName, string password, bool manager)
         {
             this.username = fInitial + lastName;
             this.password = password;
+            this.isManager = manager;
         }
+
     }
 }
