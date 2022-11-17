afc8234bf990087c6d8da78c52bb45833b19fae8
Brianna Renni <briannaxrene@icloud.com>
2022-11-16 16:15:08 -0800

    Add Web API to project

---
+++ RequestAPI/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add logging
+builder.Logging.AddSimpleConsole();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Welcome to the Request Handler API");
+
+
+
+await app.RunAsync();
